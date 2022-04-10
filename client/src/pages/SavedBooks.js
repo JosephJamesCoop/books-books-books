ab1ba2337245b52683e14c24b7c449cfe7196513
JosephJamesCoop <cooper_3869@live.com>
2022-04-10 14:14:26 -0700

    setup for heroku

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -31,10 +31,6 @@ const SavedBooks = () => {
         variables: { bookId: bookId },
       });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// import ApolloServer
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
@@ -10,26 +10,13 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers, 
-    context: authMiddleware 
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-  // Start the Apollo server
-  await server.start();
-
-  // integrate our Apollo server with the Express application as middleware
-  server.applyMiddleware({ app });
-
-  // log where we can go to test our GQL API
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-// Initialize the Apollo server
-startServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
