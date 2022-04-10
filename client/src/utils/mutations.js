d2913e7d9b985ddd35e1788d11b2161528636cb5
JosephJamesCoop <cooper_3869@live.com>
2022-04-10 10:46:01 -0700

    input mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,63 @@
-// TODO: Create LOGIN_USER
+import { gql } from '@apollo/client';
 
-// TODO: Create ADD_USER
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-// TODO: Create SAVE_BOOK
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-// TODO: Create REMOVE_BOOK
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
