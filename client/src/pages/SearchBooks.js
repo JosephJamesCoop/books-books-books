58ea66eab5c52ee61186b9bd30108fa85db48d74
JosephJamesCoop <cooper_3869@live.com>
2022-04-10 11:03:24 -0700

    imported queries and mutations into SavedBooks and SearchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
-// TODO: Use the Apollo useMutation() Hook to execute the SAVE_BOOK mutation in the handleSaveBook() function instead of the saveBook() function imported from the API file.
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -21,6 +20,9 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  const [saveBook] = useMutation(SAVE_BOOK);
+
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
