097b57526e9166d7885adb9f1d73c72e9ac32740
JosephJamesCoop <cooper_3869@live.com>
2022-04-23 10:07:50 -0700

    testing server

--- server/server.js
+++ server/server.js
@@ -1,55 +1,42 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
-const express = require('express');
-const path = require('path');
-
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
-// import typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
-});
-
 const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
   });
+  // authMiddleware is not being properly created on login or signup
 
-  // Start the Apollo server
   await server.start();
 
-  // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
-
+  console.log(
+    `Use GraphQL queries and mutations at http://localhost:${PORT}${server.graphqlPath}`
+  );
 };
 
-// Initialize the Apollo server
 startServer();
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    // log where we can go to test our GQL API
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
 });
