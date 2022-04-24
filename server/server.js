6429f1fd63c7b8c6986e40d626b15025dfccfb21
JosephJamesCoop <cooper_3869@live.com>
2022-04-24 11:25:05 -0700

    testing heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,32 +8,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
-});
-
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
   await server.start();
- server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  server.applyMiddleware({ app });
+  console.log(`Please use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 startServer();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API Server started on port${PORT}`)
+    console.log(`API Server started on port ${PORT}`);
   });
 });
