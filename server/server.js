10bdca5fa8aebe92becb8aa3472a20ed0bf96518
JosephJamesCoop <cooper_3869@live.com>
2022-04-24 13:25:09 -0700

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
-const app = express();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware,
+    context: authMiddleware
   });
+  // authMiddleware is not being properly created on login or signup
+
   await server.start();
+
   server.applyMiddleware({ app });
-  console.log(`Please use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  console.log(
+    `Use GraphQL queries and mutations at http://localhost:${PORT}${server.graphqlPath}`
+  );
 };
 
 startServer();
@@ -24,15 +31,11 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API Server started on port ${PORT}`);
   });
